5b86bcac12b28f951a42be70001296ea00df21db
sudheerreddy93 <143996646+sudheerreddy93@users.noreply.github.com>
2023-10-28 18:22:12 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSServer"{
+resource "aws_instance" "AWSInstance"{
 ami = "ami-06791f9213cbb608b"
 instance_type = "t2.micro"
 key_name = "devopstraning"
